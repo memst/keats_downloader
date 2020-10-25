d17402a870064c98a92b892f4682f57fcfec7ba9
memst <stankev.martynas@gmail.com>
2020-10-25 23:47:50 +0200

    Removed unnecessary files, created a new temp structure for easy execution.

--- video_links.js
+++
@@ -1,6 +0,0 @@
-videoEntries = content.getElementsByClassName("activity kalvidres modtype_kalvidres ")
-	console.log(videoEntries)
-
-	for (entry of videoEntries) {
-		link = entry.getElementsByClassName("aalink")[0].getAttribute("href")
-	}
--- video_page.js
+++
@@ -1,3 +0,0 @@
-video = document.getElementsByClassName("persistentNativePlayer nativeEmbedPlayerPid")[0]
-videoUrl = video.getAttribute("src")
-subtitleUrl = video.getElementsByTagName("track")[0].getAttribute("src")
